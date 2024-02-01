5cd5c443cb4a57ad7d67e61ecc0bb89210e8f9f3
heni_ltaief <heniltaief@gmail.com>
2024-02-01 12:58:02 +0100

    switch counter logic to the parent

--- vite-project/src/components/CounterList.jsx
+++ vite-project/src/components/CounterList.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./Counter";
 
 const CounterList = () => {
-  const objectArray = [
-    { id: 1, title: "title 1", value: 10 },
-    { id: 2, title: "title 2", value: 20 },
-    { id: 3, title: "title 3", value: 30 },
-    { id: 4, title: "title 4", value: 40 },
-    { id: 5, title: "title 5", value: 50 },
-  ];
+  //make the objectArray as a state to be able to manage its values
+  const [counterArray, setCounterArray] = useState([
+    { id: 1, title: "title 1", value: 0 },
+    { id: 2, title: "title 2", value: 0 },
+    { id: 3, title: "title 3", value: 0 },
+    { id: 4, title: "title 4", value: 0 },
+    { id: 5, title: "title 5", value: 0 },
+  ]);
+
+  //Move counter handlers in parent component to be able to manage the values dynamically
+  const handleIncrement = (id) => {
+    setCounterArray((currentArray) => {
+      //Loop through the counterArray state to manage each value by id
+      currentArray.map((item) =>
+        //Check if the id clicked equals the same id in currentArray than increment else nothing
+        item.id === id ? { ...item, value: item.value + 1 } : item
+      );
+    });
+  };
+
+  const handleDecrement = (id) => {
+    setCounterArray((currentArray) => {
+      //Loop through the counterArray state to manage each value by id
+      currentArray.map((item) =>
+        //Check if the id clicked equals the same id in currentArray than decrement else nothing
+        item.id === id ? { ...item, value: item.value - 1 } : item
+      );
+    });
+  };
 
   return (
     <div className="d-flex flex-wrap">
-      {objectArray.map((item) => {
+      {counterArray.map((item) => {
         return (
           <Counter key={item.id} value={item.value}>
             {item.title}
@@ -22,6 +44,5 @@ const CounterList = () => {
     </div>
   );
 };
-// values => dynamic
-// App.jsx => sum of values
+
 export default CounterList;
